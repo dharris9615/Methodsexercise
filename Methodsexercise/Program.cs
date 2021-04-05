63332be8eea94c7a1699ab9bd55da4495b5bd91c
David Harris <davlsu14@gmail.com>
2021-04-05 14:33:51 -0500

    finished part 2

--- Methodsexercise/Program.cs
+++ Methodsexercise/Program.cs
@@ -4,8 +4,26 @@ namespace Methodsexercise
 {
     class Program
     {
+        public static int Sum(int num1, int num2)
+        {
+            var answer = num1 + num2;
+            return answer;
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+
+
         static void Main(string[] args)
         {
+
+            var amountOfTeams = Sum(2, 6);
+            var money = Multiply(3, 10, 5);
+
+
+
             Console.WriteLine("Hello What is your name?");
             var userName = Console.ReadLine();
 
